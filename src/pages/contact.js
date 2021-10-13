6f1d7816ba6ec7db942a6965a72d1471b4d4d302
Kehinde Onifade <onifkay@gmail.com>
2021-10-13 20:53:08 +0100

    worked on responsiveness

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,7 @@ import React from 'react'
 const contact = () => {
   return (
     <div className="h-screen flex justify-center items-center bg-green-400">
-      <h1 className="text-9xl uppercase font-black">
+      <h1 className="lg:text-9xl md:text-7xl sm:text-5xl text-3xl uppercase font-black">
         Contact
       </h1>
     </div>
